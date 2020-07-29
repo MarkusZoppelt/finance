d981dde22eab034e16410277f628ac89f14f53b4
Markus Zoppelt <markus@zoppelt.net>
2020-07-29 14:34:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Finance
+
+A simple command line tool to view and edit financial investment portfolios
+
+Install requirements:
+
+    pip install -r requirements.txt
+
+Usage:
+
+    ./portfolio.py <data.csv>
+
+Example data can be found in `example_data.csv`
---
+++ portfolio.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+import sys
+import getopt
+import pandas as pd
+from pandas_datareader.data import get_quote_yahoo
+from termcolor import colored
+
+if len(sys.argv) == 1:
+    print("No args given, using example data...")
+    data = pd.read_csv("example_data.csv")
+else:
+    data = pd.read_csv(sys.argv[1])
+
+
+def getQuotes():
+    tickers = data["Ticker"]
+
+    print("Getting current prices from Yahoo Finance...")
+    balances = []
+
+    for i in range(0, len(tickers)):
+        ac = data["AC"][i]
+        # If Asset Class is cash, then get amount only
+        if ac == "CASH" or ac == "P2P":
+            balances.append(data["Amount"][i])
+        else:
+            try:
+                currPrice = get_quote_yahoo(tickers[i])["price"][0]
+                amount = data["Amount"][i]
+                balances.append(round(currPrice*amount, 2))
+            except Exception:
+                print("No quote for this ticker")
+    data["Balance"] = balances
+
+
+def calculateTotalBalance():
+    totalBalance = 0.0
+    for i in range(0, len(data)):
+        totalBalance += data["Balance"][i]
+    return totalBalance
+
+
+def analyzeAC():
+    stocks, bonds, commodities, gold, crypto, p2p, cash = 0,0,0,0,0,0,0
+
+    for i in range(0, len(data)):
+        ac = data["AC"][i]
+
+        if ac == "S":
+            stocks += data["Balance"][i]
+        elif ac == "B":
+            bonds += data["Balance"][i]
+        elif ac == "C":
+            commodities += data["Balance"][i]
+        elif ac == "G":
+            gold += data["Balance"][i]
+        elif ac == "CRYPTO":
+            crypto += data["Balance"][i]
+        elif ac == "P2P":
+            p2p += data["Balance"][i]
+        elif ac == "CASH":
+            cash += data["Balance"][i]
+
+    totalBalance = calculateTotalBalance()
+
+    pctStocks = stocks/totalBalance * 100
+    pctBonds = bonds/totalBalance * 100
+    pctCommodities = commodities/totalBalance * 100
+    pctGold = gold/totalBalance * 100
+    pctCrypto = crypto/totalBalance * 100
+    pctP2P = p2p/totalBalance * 100
+    pctCash = cash/totalBalance * 100
+
+    cryptoStr = ""
+    if pctCrypto < 1 or pctCrypto > 10:
+        cryptoStr = colored(str(round(pctCrypto, 2))+"%", "red") + " (optimal: 1% - 10%)"
+    else: 
+        cryptoStr = str(round(pctCrypto, 2))+"%"
+
+    commoditiesStr = ""
+    if pctCommodities > 7.5:
+        commoditiesStr = colored(str(round(pctCommodities, 2))+"%", "red") + " (optimal: 0% - 7.5%)"
+    else: 
+        commoditiesStr = str(round(pctCommodities, 2))+"%"
+    
+    goldStr = ""
+    if pctGold < 5 or pctGold > 10:
+        goldStr = colored(str(round(pctGold, 2))+"%", "red") + " (optimal: 5% - 10%)"
+    else: 
+        goldStr = str(round(pctGold, 2))+"%"
+
+    p2pStr = ""
+    if pctP2P > 10:
+        p2pStr = colored(str(round(pctP2P, 2))+"%", "red") + " (optimal: 0% - 10%)"
+    else: 
+        p2pStr = str(round(pctP2P, 2))+"%"
+
+    print("Percentage of stocks: " + str(round(pctStocks, 2))+"%")
+    print("Percentage of bonds: " + str(round(pctBonds, 2))+"%")
+    print("Percentage of commodities: " + commoditiesStr)
+    print("Percentage of gold: " + goldStr)
+    print("Percentage of crypto: " + cryptoStr)
+    print("Percentage of P2P: " + p2pStr)
+    print("Percentage of cash: " + str(round(pctCash, 2))+"%")
+    print("----------------------------------------------------------------")
+    print("Prepared for market situations:")
+    print("Normal: " + str(round(pctStocks, 2)))
+    print("Inflation: "+ str(round(pctCommodities + pctGold + pctCrypto, 2)))
+    print("Deflation: "+ str(round(pctBonds + pctCash + pctP2P, 2)))
+
+    print("================================================================")
+
+
+def main():
+    getQuotes()
+    cmd = ""
+
+    while cmd != "q":
+        cmd = input(
+            'Hello Sir, what do you want to do? (a)nalyze, (b)alance, (q)uit: ')
+        print("================================================================")
+
+        if cmd == "a":
+            analyzeAC()
+        elif cmd == "b":
+            print(data)
+            print("Your total balance is: " +
+                  str(round(calculateTotalBalance(), 2))+"€")
+            print("================================================================")
+
+if __name__ == "__main__":
+    main()
