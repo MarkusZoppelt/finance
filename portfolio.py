e587797a1d03d457fa64504f6c5c2deb122fc5b1
Markus Zoppelt <markus@zoppelt.net>
2022-04-24 13:41:47 +0200

    remove P2P AC and termcolor

--- portfolio.py
+++ portfolio.py
@@ -3,7 +3,6 @@
 import sys
 import pandas as pd
 from pandas_datareader.data import get_quote_yahoo
-from termcolor import colored
 
 import reports
 
@@ -30,7 +29,7 @@ def getQuotes(data):
     for i in range(0, len(tickers)):
         ac = data["AC"][i]
         # If Asset Class is cash, then get amount only
-        if ac == "CASH" or ac == "P2P":
+        if ac == "CASH":
             balances.append(data["Amount"][i])
         else:
             try:
@@ -42,27 +41,6 @@ def getQuotes(data):
     data["Balance"] = balances
     return data
 
-# TODO
-def getPerformance():
-    tickers = data["Ticker"]
-
-    startBalances = []
-
-    for i in range(0, len(tickers)):
-        ac = data["AC"][i]
-        # If Asset Class is cash, then get amount only
-        if ac == "CASH" or ac == "P2P":
-            startBalances.append(data["Amount"][i])
-        else:
-            try:
-                startPrice = get_quote_yahoo(tickers[i])["price"][0]
-                amount = data["Amount"][i]
-                startBalances.append(round(startPrice*amount, 2))
-            except Exception:
-                print("No quote for this ticker")
-    data["1.1"] = startBalances
-
-
 def getTotalBalance():
     totalBalance = 0.0
     for i in range(0, len(data)):
@@ -71,8 +49,8 @@ def getTotalBalance():
 
 
 def getAssetAllocation():
-    labels = "Stocks", "Bonds", "Commodities", "Gold", "Crypto", "P2P", "Cash"
-    stocks, bonds, commodities, gold, crypto, p2p, cash = 0, 0, 0, 0, 0, 0, 0
+    labels = "Stocks", "Bonds", "Commodities", "Gold", "Crypto", "Cash"
+    stocks, bonds, commodities, gold, crypto, cash = 0, 0, 0, 0, 0, 0
     for i in range(0, len(data)):
         ac = data["AC"][i]
 
@@ -86,11 +64,9 @@ def getAssetAllocation():
             gold += data["Balance"][i]
         elif ac == "CRYPTO":
             crypto += data["Balance"][i]
-        elif ac == "P2P":
-            p2p += data["Balance"][i]
         elif ac == "CASH":
             cash += data["Balance"][i]
-    return labels, (stocks, bonds, commodities, gold, crypto, p2p, cash)
+    return labels, (stocks, bonds, commodities, gold, crypto, cash)
 
 def getAllocationForClass(c):
     cdata = data.loc[data['AC'] == c]
@@ -102,7 +78,7 @@ def getAllocationForClass(c):
     return labels, ticker_balances
 
 def analyzeAC():
-    stocks, bonds, commodities, gold, crypto, p2p, cash = getAssetAllocation()[1]
+    stocks, bonds, commodities, gold, crypto, cash = getAssetAllocation()[1]
     totalBalance = getTotalBalance()
 
     pctStocks = stocks/totalBalance * 100
@@ -110,50 +86,15 @@ def analyzeAC():
     pctCommodities = commodities/totalBalance * 100
     pctGold = gold/totalBalance * 100
     pctCrypto = crypto/totalBalance * 100
-    pctP2P = p2p/totalBalance * 100
     pctCash = cash/totalBalance * 100
 
-    cryptoStr = ""
-    if pctCrypto < 1 or pctCrypto > 10:
-        cryptoStr = colored(str(round(pctCrypto, 2))+"%",
-                            "red") + " (optimal: 1% - 10%)"
-    else:
-        cryptoStr = str(round(pctCrypto, 2))+"%"
-
-    commoditiesStr = ""
-    if pctCommodities > 7.5:
-        commoditiesStr = colored(
-            str(round(pctCommodities, 2))+"%", "red") + " (optimal: 0% - 7.5%)"
-    else:
-        commoditiesStr = str(round(pctCommodities, 2))+"%"
-
-    goldStr = ""
-    if pctGold < 5 or pctGold > 10:
-        goldStr = colored(str(round(pctGold, 2))+"%",
-                          "red") + " (optimal: 5% - 10%)"
-    else:
-        goldStr = str(round(pctGold, 2))+"%"
-
-    p2pStr = ""
-    if pctP2P > 10:
-        p2pStr = colored(str(round(pctP2P, 2))+"%", "red") + \
-            " (optimal: 0% - 10%)"
-    else:
-        p2pStr = str(round(pctP2P, 2))+"%"
 
     print("Percentage of stocks: " + str(round(pctStocks, 2))+"%")
     print("Percentage of bonds: " + str(round(pctBonds, 2))+"%")
-    print("Percentage of commodities: " + commoditiesStr)
-    print("Percentage of gold: " + goldStr)
-    print("Percentage of crypto: " + cryptoStr)
-    print("Percentage of P2P: " + p2pStr)
+    print("Percentage of commodities: " + str(round(pctCommodities, 2))+"%")
+    print("Percentage of gold: " + str(round(pctGold, 2))+"%")
+    print("Percentage of crypto: " + str(round(pctCrypto, 2))+"%")
     print("Percentage of cash: " + str(round(pctCash, 2))+"%")
-    print("----------------------------------------------------------------")
-    print("Prepared for market situations:")
-    print("Normal: " + str(round(pctStocks, 2)))
-    print("Inflation: " + str(round(pctCommodities + pctGold + pctCrypto, 2)))
-    print("Deflation: " + str(round(pctBonds + pctCash + pctP2P, 2)))
-
     print("================================================================")
 
 def getDataFrame(filename):
@@ -171,7 +112,7 @@ def main():
         '\033[1m' + '(q)'+'\033[0m'+'uit'        # quit
     ]
 
-    greetingText = '\033[1m' + "Hello Sir, what do you want to do?" + '\033[0m' + " (Choose option and hit Enter)\n"
+    greetingText = '\033[1m' + "Hello, what do you want to do?" + '\033[0m' + " (Choose option and hit Enter)\n"
     for co in cmdOptions:
            greetingText += "  " + co+"\n"
     greetingText += ">"
